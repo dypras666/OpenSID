14f08cee43a07220ddad21869b32a9e2763bfc89
Eddie Ridwan <eddie.ridwan@gmail.com>
2020-07-03 16:47:52 +1000

    Seragamkan paginasi halaman C-Desa

--- donjo-app/views/data_persil/c_desa.php
+++ donjo-app/views/data_persil/c_desa.php
@@ -99,73 +99,16 @@
 												</div>
 											</div>
 										</form>
-											<div class="row">
-												<div class="col-sm-6">
-													<div class="dataTables_length">
-														<form id="paging" action="<?= site_url("cdesa")?>" method="post" class="form-horizontal">
-															<label>
-																Tampilkan
-																<select name="per_page" class="form-control input-sm" onchange="$('#paging').submit()">
-																	<option value="20" <?php selected($per_page, 20); ?> >20</option>
-																	<option value="50" <?php selected($per_page, 50); ?> >50</option>
-																	<option value="100" <?php selected($per_page, 100); ?> >100</option>
-																</select>
-																Dari
-																<strong><?= $paging->num_rows?></strong>
-																Total Data
-															</label>
-														</form>
-													</div>
-												</div>
-												<div class="col-sm-6">
-													<div class="dataTables_paginate paging_simple_numbers">
-														<ul class="pagination">
-															<?php if ($paging->start_link): ?>
-																<li><a href="<?= site_url("cdesa/index/$paging->start_link/$o")?>" aria-label="First"><span aria-hidden="true">Awal</span></a></li>
-															<?php endif; ?>
-															<?php if ($paging->prev): ?>
-																<li><a href="<?= site_url("cdesa/index/$paging->prev/$o")?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-															<?php endif; ?>
-															<?php for ($i=$paging->start_link;$i<=$paging->end_link;$i++): ?>
-																<li <?=jecho($p, $i, "class='active'")?>><a href="<?= site_url("data_persil/index/$kat/$mana/$i/$o")?>"><?= $i?></a></li>
-															<?php endfor; ?>
-															<?php if ($paging->next): ?>
-																<li><a href="<?= site_url("cdesa/index/$paging->next/$o")?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
-															<?php endif; ?>
-															<?php if ($paging->end_link): ?>
-																<li><a href="<?= site_url("cdesa/index/$paging->end_link/$o")?>" aria-label="Last"><span aria-hidden="true">Akhir</span></a></li>
-															<?php endif; ?>
-														</ul>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class='modal fade' id='confirm-delete' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-									<div class='modal-dialog'>
-										<div class='modal-content'>
-											<div class='modal-header'>
-												<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-												<h4 class='modal-title' id='myModalLabel'><i class='fa fa-exclamation-triangle text-red'></i> Konfirmasi</h4>
-											</div>
-											<div class='modal-body btn-info'>
-												Apakah Anda yakin ingin menghapus data ini?
-											</div>
-											<div class='modal-footer'>
-												<button type="button" class="btn btn-social btn-flat btn-warning btn-sm" data-dismiss="modal"><i class='fa fa-sign-out'></i> Tutup</button>
-												<a class='btn-ok'>
-													<button type="button" class="btn btn-social btn-flat btn-danger btn-sm" id="ok-delete"><i class='fa fa-trash-o'></i> Hapus</button>
-												</a>
-											</div>
-										</div>
+										<?php $this->load->view('global/paging');?>
 									</div>
 								</div>
 							</div>
+							<?php $this->load->view('global/confirm_delete');?>
 						</div>
 					</div>
 				</div>
-			</form>
-		</section>
-	</div>
+			</div>
+		</form>
+	</section>
+</div>
 
